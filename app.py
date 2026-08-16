26fc75d191b5b6e23d1cb8d48160ad58d73d011b
earneylasten <56005285+earneylasten@users.noreply.github.com>
2026-08-15 19:10:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="PlanningMind · ViaMoon",
-    page_icon="🌙",
+    page_icon="https://exhibz.viamoon.com/wp-content/uploads/2026/08/cropped-Favicon114.png",
     layout="wide",
 )
 
